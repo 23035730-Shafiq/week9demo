cb464ff268fd502c7eabe9c76f1f70d5dbd0a929
23035730-Shafiq <23035730@myrp.edu.sg>
2026-01-13 01:52:06 +0800

    commit all files

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
-import React,{useEffect,useState} from 'react';
-import { FlatList, StatusBar, Text, TextInput, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StatusBar, Text, TextInput, View } from 'react-native';
 
 const App = () => {
     const [myData, setMyData] = useState([]);
 
-    //Add fetch() - Exercise 1A
-    const myurl = "https://onlinecardappwebservice-cz5e.onrender.com/allcards"
-    useEffect(()=>{
+    const myurl = "https://week9demo.onrender.com/payments";
+
+    useEffect(() => {
         fetch(myurl)
-            .then((response) => {
-                return response.json();
-            })
-            .then((myJson)=>{
-                setMyData(myJson);
-            });
-    },[]);
+            .then(response => response.json())
+            .then(json => setMyData(json))
+            .catch(err => console.log(err));
+    }, []);
 
-    const renderItem = ({item, index}) => {
-        return (
-            <View>
-                <Text style={{borderWidth:1}}>{item.card_name}</Text>
-            </View>
-        );
-    };
-
-    return (
-        <View>
-            <StatusBar/>
-            <Text>Search:</Text>
-            <TextInput style={{borderWidth:1}}/>
-            <FlatList data={myData} renderItem={renderItem} />
+    const renderItem = ({ item }) => (
+        <View style={{ borderWidth: 1, padding: 8, marginBottom: 6 }}>
+            <Text>Payment ID: {item.paymentid}</Text>
+            <Text>Amount: {item.amount}</Text>
+            <Text>Status: {item.payment_status}</Text>
+            <Text>Method: {item.payment_method}</Text>
         </View>
     );
-}
+
+    return (
+        <View style={{ padding: 12 }}>
+            <StatusBar />
+            <Text>Search:</Text>
+            <TextInput style={{ borderWidth: 1, marginBottom: 10, padding: 6 }} />
+
+            <FlatList
+                data={myData}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.paymentid.toString()}
+            />
+        </View>
+    );
+};
 
 export default App;
